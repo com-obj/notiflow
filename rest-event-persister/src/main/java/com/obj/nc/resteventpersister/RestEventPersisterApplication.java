33ef63abf033ddd655e62898beab9c1cd9bf6bb1
jancuzy <cuzy@objectify.sk>
2020-09-28 10:15:20 +0200

    initial commit

---
+++ rest-event-persister/src/main/java/com/obj/nc/resteventpersister/RestEventPersisterApplication.java
@@ -0,0 +1,13 @@
+package com.obj.nc.resteventpersister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEventPersisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEventPersisterApplication.class, args);
+	}
+
+}
---
+++ rest-event-persister/src/test/java/com/obj/nc/resteventpersister/RestEventPersisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obj.nc.resteventpersister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEventPersisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
