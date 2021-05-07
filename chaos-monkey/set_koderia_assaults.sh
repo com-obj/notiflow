76fa159f4d1455b9f90a289494ddbdb3c96f95cc
Peter Bazik <bazik@objectify.sk>
2021-05-07 12:07:54 +0200

    try koderia-flows exception invoking by chaosmonkey

--- chaos-monkey/set_koderia_assaults.sh
+++ chaos-monkey/set_koderia_assaults.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 /bin/bash ./set_assaults.sh '{
-  "level": 2,
+  "level": 1,
   "latencyRangeStart": 2000,
-  "latencyRangeEnd": 6000,
+  "latencyRangeEnd": 5000,
   "latencyActive": true,
   "exceptionsActive": true,
+  "exception": {
+    "type": "java.lang.IllegalArgumentException",
+    "arguments": [{
+      "className": "java.lang.String",
+      "value": "custom illegal argument exception"}] },
   "killApplicationActive": false,
   "runtimeAssaultCronExpression": "OFF",
-  "watchedCustomServices": []
+  "watchedCustomServices": ["com.obj.nc.koderia.functions.processors.recipientsFinder.KoderiaRecipientsFinder.apply"]
 }'
