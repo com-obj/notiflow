eb975d6405d973454628c713fa5f9f9af7907d87
Peter Bazik <bazik@objectify.sk>
2021-06-14 17:25:59 +0200

    drop endpoint name in nc_message, nc_delivery_info

--- functions/src/main/resources/db/migration/V0_0_21__alter_nc_endpoint_id.sql
+++ functions/src/main/resources/db/migration/V0_0_21__alter_nc_endpoint_id.sql
@@ -8,9 +8,11 @@ alter table nc_endpoint add constraint nc_endpoint_endpoint_id_unique_key unique
 alter table nc_message rename column endpoint_ids to endpoint_names;
 alter table nc_message add column endpoint_ids uuid[];
 update nc_message as nm set endpoint_ids = array(select ep.id from nc_endpoint ep where ep.endpoint_name = any(nm.endpoint_names)) where endpoint_ids is null;
+alter table nc_message drop column endpoint_names;
 
 alter table nc_delivery_info rename column endpoint_id to endpoint_name;
 alter table nc_delivery_info add column endpoint_id uuid;
 update nc_delivery_info as di set endpoint_id = ep.id from nc_endpoint ep where di.endpoint_name = ep.endpoint_name and di.endpoint_id is null;
+alter table nc_delivery_info drop column endpoint_name;
 
 alter table nc_endpoint drop column endpoint_name;
