58fdfc182db1f1d91653a88d14950cf26b3c0fe0
Peter Bazik <bazik@objectify.sk>
2021-07-20 13:49:03 +0200

    add delivery infos counts per delivery status to EndpointDto

--- src/main/java/com/obj/nc/controllers/EndpointsRestController.java
+++ src/main/java/com/obj/nc/controllers/EndpointsRestController.java
@@ -1,6 +1,9 @@
 package com.obj.nc.controllers;
 
 import com.obj.nc.domain.endpoints.RecievingEndpoint;
+import com.obj.nc.functions.processors.deliveryInfo.domain.DeliveryInfo;
+import com.obj.nc.functions.processors.deliveryInfo.domain.DeliveryInfo.DELIVERY_STATUS;
+import com.obj.nc.repositories.DeliveryInfoRepository;
 import com.obj.nc.repositories.EndpointsRepository;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toCollection;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Validated
@@ -21,12 +25,21 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/endpoints")
 @RequiredArgsConstructor
 public class EndpointsRestController {
-    private final EndpointsRepository endpointRepo;
+    private final EndpointsRepository endpointRepository;
+    private final DeliveryInfoRepository deliveryInfoRepository;
     
     @GetMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public List<EndpointDto> findAllEndpoints() {
-        List<RecievingEndpoint> receivingEndpoints = endpointRepo.findAll();
-        List<EndpointDto> endpointDtos = EndpointDto.createFrom(receivingEndpoints);
+        List<RecievingEndpoint> receivingEndpoints = endpointRepository.findAll();
+        
+        List<EndpointDto> endpointDtos = EndpointDto.from(receivingEndpoints);
+        endpointDtos.forEach(
+                endpointDto -> {
+                    List<DeliveryInfo> endpointDeliveryInfos = deliveryInfoRepository.findByEndpointIdOrderByProcessedOn(endpointDto.getId());
+                    List<MessagesPerStatus> messagesPerStatuses = MessagesPerStatus.from(endpointDeliveryInfos);
+                    endpointDto.setMessagesPerStatus(messagesPerStatuses);
+                });
+        
         return endpointDtos;
     }
     
@@ -36,8 +49,9 @@ public class EndpointsRestController {
         private UUID id;
         private String name;
         private String type;
+        private List<MessagesPerStatus> messagesPerStatus;
     
-        public static List<EndpointDto> createFrom(List<RecievingEndpoint> receivingEndpoints) {
+        public static List<EndpointDto> from(List<RecievingEndpoint> receivingEndpoints) {
             return receivingEndpoints.stream()
                     .map(
                             receivingEndpoint -> EndpointDto.builder()
@@ -47,6 +61,49 @@ public class EndpointsRestController {
                                 .build()
                     )
                     .collect(Collectors.toList());
+        }
+    }
+    
+    @Data
+    @Builder
+    public static class MessagesPerStatus {
+        private DELIVERY_STATUS status;
+        private Integer count;
+        
+        public static List<MessagesPerStatus> from(List<DeliveryInfo> deliveryInfos) {
+            Map<UUID, LinkedList<DeliveryInfo>> infosByMessage = groupInfosByMessage(deliveryInfos);
+            
+            Map<DELIVERY_STATUS, Integer> messagesPerStatus = countMessagesPerStatus(infosByMessage);
+    
+            return messagesPerStatus.entrySet().stream()
+                    .map(
+                            messagesPerStatusEntry -> MessagesPerStatus.builder()   
+                            .status(messagesPerStatusEntry.getKey())
+                            .count(messagesPerStatusEntry.getValue())
+                            .build()
+                    )
+                    .collect(Collectors.toList());
+        }
+    
+        private static Map<DELIVERY_STATUS, Integer> countMessagesPerStatus(Map<UUID, LinkedList<DeliveryInfo>> infosByMessage) {
+            Map<DELIVERY_STATUS, Integer> messagesPerStatus = new HashMap<>();
+            
+            infosByMessage.values().forEach(
+                    (infos) -> {
+                        DeliveryInfo lastInfo = infos.getLast();
+                        if (!messagesPerStatus.containsKey(lastInfo.getStatus())) {
+                            messagesPerStatus.put(lastInfo.getStatus(), 0);
+                        }
+                        messagesPerStatus.put(lastInfo.getStatus(), messagesPerStatus.get(lastInfo.getStatus()) + 1);
+                    }
+            );
+            
+            return messagesPerStatus;
+        }
+    
+        private static Map<UUID, LinkedList<DeliveryInfo>> groupInfosByMessage(List<DeliveryInfo> deliveryInfos) {
+            return deliveryInfos.stream()
+                    .collect(groupingBy(DeliveryInfo::getMessageId, LinkedHashMap::new, toCollection(LinkedList::new)));
         }
     }
 }
--- src/test/java/com/obj/nc/controllers/EndpointsRestControllerTest.java
+++ src/test/java/com/obj/nc/controllers/EndpointsRestControllerTest.java
@@ -1,25 +1,31 @@
 package com.obj.nc.controllers;
 
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
 import com.jayway.jsonpath.JsonPath;
-import com.obj.nc.controllers.DeliveryInfoRestController.EndpointDeliveryInfoDto;
 import com.obj.nc.domain.endpoints.EmailEndpoint;
 import com.obj.nc.domain.endpoints.SmsEndpoint;
 import com.obj.nc.domain.message.EmailMessage;
 import com.obj.nc.domain.message.MessagePersistantState;
+import com.obj.nc.flows.messageProcessing.MessageProcessingFlow;
 import com.obj.nc.functions.processors.deliveryInfo.domain.DeliveryInfo;
 import com.obj.nc.functions.processors.deliveryInfo.domain.DeliveryInfo.DELIVERY_STATUS;
+import com.obj.nc.repositories.DeliveryInfoRepository;
 import com.obj.nc.repositories.EndpointsRepository;
+import com.obj.nc.repositories.MessageRepository;
 import com.obj.nc.testUtils.BaseIntegrationTest;
 import com.obj.nc.testUtils.SystemPropertyActiveProfileResolver;
-import org.assertj.core.api.Assertions;
+import com.obj.nc.utils.JsonUtils;
 import org.awaitility.Awaitility;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.integration.test.context.SpringIntegrationTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
@@ -28,13 +34,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
+import java.time.Duration;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 import static com.obj.nc.flows.inputEventRouting.config.InputEventRoutingFlowConfig.GENERIC_EVENT_CHANNEL_ADAPTER_BEAN_NAME;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,8 +47,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class EndpointsRestControllerTest extends BaseIntegrationTest {
 	
-	@Autowired private EndpointsRepository endpointRepo;
+	@Autowired private EndpointsRepository endpointRepository;
+	@Autowired private DeliveryInfoRepository deliveryInfoRepository;
+	@Autowired private MessageRepository messageRepository;
 	@Autowired private MockMvc mockMvc;
+	@Autowired private MessageProcessingFlow messageProcessingFlow;
+	
+	@RegisterExtension
+	protected static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+			.withConfiguration(
+					GreenMailConfiguration.aConfig()
+							.withUser("no-reply@objectify.sk", "xxx"))
+			.withPerMethodLifecycle(true);
 
     @BeforeEach
     void setUp(@Autowired JdbcTemplate jdbcTemplate) {
@@ -55,12 +66,12 @@ class EndpointsRestControllerTest extends BaseIntegrationTest {
     }
     
     @Test
-    void testFindDeliveryInfosRest() throws Exception {
+    void testFindAllEndpoints() throws Exception {
     	//GIVEN
     	EmailEndpoint email = EmailEndpoint.builder().email("john.doe@objectify.sk").build();
-		endpointRepo.persistEnpointIfNotExists(email);
+		endpointRepository.persistEnpointIfNotExists(email);
 		SmsEndpoint phone = SmsEndpoint.builder().phone("+999999999999").build();
-    	endpointRepo.persistEnpointIfNotExists(phone);
+    	endpointRepository.persistEnpointIfNotExists(phone);
     	
     	//WHEN TEST REST
         ResultActions resp = mockMvc
@@ -83,6 +94,37 @@ class EndpointsRestControllerTest extends BaseIntegrationTest {
 	
 		List<EndpointsRestController.EndpointDto> endpoints = JsonPath.read(resp.andReturn().getResponse().getContentAsString(), "$");
 		assertThat(endpoints).hasSize(2);
+	}
+	
+	@Test
+	void testCountMessagesPerStatus() throws Exception {
+		//GIVEN
+		EmailMessage message = JsonUtils.readObjectFromClassPathResource("messages/simple_email_message.json", EmailMessage.class);
+		messageProcessingFlow.processMessage(message);
+		awaitDeliveryInfos();
+		
+		//WHEN
+		ResultActions resp = mockMvc
+				.perform(MockMvcRequestBuilders.get("/endpoints")
+						.contentType(APPLICATION_JSON_UTF8)
+						.accept(APPLICATION_JSON_UTF8))
+				.andDo(MockMvcResultHandlers.print());
+		
+		//THEN
+		resp
+				.andExpect(status().is2xxSuccessful())
+				
+				.andExpect(jsonPath("$[0].messagesPerStatus[0].status").value(CoreMatchers.is("SENT")))
+				.andExpect(jsonPath("$[0].messagesPerStatus[0].count").value(CoreMatchers.is(1)));
+				
+		List<EndpointsRestController.EndpointDto> endpoints = JsonPath.read(resp.andReturn().getResponse().getContentAsString(), "$");
+		assertThat(endpoints).hasSize(1);
+	}
+	
+	private void awaitDeliveryInfos() {
+		Awaitility.await().atMost(Duration.ofSeconds(3)).until(() -> messageRepository.findAll().iterator().next() != null);
+		MessagePersistantState sentMessage = messageRepository.findAll().iterator().next();
+		Awaitility.await().atMost(Duration.ofSeconds(3)).until(() -> deliveryInfoRepository.findByMessageIdOrderByProcessedOn(sentMessage.getId()).size() == 2);
 	}
 	
 }
