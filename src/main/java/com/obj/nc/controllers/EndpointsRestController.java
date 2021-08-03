6db4868655722f6531ddd0314f20e72e19df1fcc
Peter Bazik <bazik@objectify.sk>
2021-08-03 15:47:01 +0200

    iso date format

--- src/main/java/com/obj/nc/controllers/EndpointsRestController.java
+++ src/main/java/com/obj/nc/controllers/EndpointsRestController.java
@@ -6,6 +6,7 @@ import com.obj.nc.services.EndpointsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,8 +24,8 @@ public class EndpointsRestController {
     
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public Page<EndpointDto> findAllEndpoints(
-            @RequestParam(value = "startAt", required = false) Instant startAt,
-            @RequestParam(value = "endAt", required = false) Instant endAt,
+            @RequestParam(value = "startAt", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant startAt,
+            @RequestParam(value = "endAt", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant endAt,
             @RequestParam(value = "endpointType", required = false) EndpointType endpointType,
             Pageable pageable) {
         return endpointsService.findAllEndpoints(startAt, endAt, endpointType, pageable);
