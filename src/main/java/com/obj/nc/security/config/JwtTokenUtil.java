4ad0d17a92946ca66c0b8d276e619e5b7d8c3c71
Peter Bazik <bazik@objectify.sk>
2021-08-23 11:36:00 +0200

    optimise imports

--- src/main/java/com/obj/nc/security/config/JwtTokenUtil.java
+++ src/main/java/com/obj/nc/security/config/JwtTokenUtil.java
@@ -5,10 +5,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
 
 import java.io.Serializable;
 import java.util.Date;
--- src/main/java/com/obj/nc/security/config/WebSecurityConfig.java
+++ src/main/java/com/obj/nc/security/config/WebSecurityConfig.java
@@ -2,15 +2,12 @@ package com.obj.nc.security.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity
