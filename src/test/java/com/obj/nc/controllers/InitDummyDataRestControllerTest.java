37389edb956ba4ac81e9a788ee038fe07f94470e
Peter Bazik <bazik@objectify.sk>
2021-08-24 12:17:36 +0200

    create data with processing infos

--- src/test/java/com/obj/nc/controllers/InitDummyDataRestControllerTest.java
+++ src/test/java/com/obj/nc/controllers/InitDummyDataRestControllerTest.java
@@ -1,18 +1,15 @@
 package com.obj.nc.controllers;
 
-import com.obj.nc.controllers.InitDummyDataRestController.DummyEventPayload;
 import com.obj.nc.domain.endpoints.RecievingEndpoint;
 import com.obj.nc.domain.event.GenericEvent;
-import com.obj.nc.domain.message.EmailMessage;
+import com.obj.nc.domain.headers.ProcessingInfo;
 import com.obj.nc.domain.message.MessagePersistantState;
-import com.obj.nc.domain.message.SmsMessage;
 import com.obj.nc.domain.notifIntent.NotificationIntent;
 import com.obj.nc.flows.errorHandling.domain.FailedPaylod;
 import com.obj.nc.functions.processors.deliveryInfo.domain.DeliveryInfo;
 import com.obj.nc.repositories.*;
 import com.obj.nc.testUtils.BaseIntegrationTest;
 import com.obj.nc.testUtils.SystemPropertyActiveProfileResolver;
-import com.obj.nc.utils.JsonUtils;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,7 +26,6 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.obj.nc.flows.inputEventRouting.config.InputEventRoutingFlowConfig.GENERIC_EVENT_CHANNEL_ADAPTER_BEAN_NAME;
-import static com.obj.nc.functions.processors.deliveryInfo.domain.DeliveryInfo.DELIVERY_STATUS.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,6 +43,7 @@ class InitDummyDataRestControllerTest extends BaseIntegrationTest {
 	@Autowired private MessageRepository messageRepository;
 	@Autowired private DeliveryInfoRepository deliveryInfoRepository;
 	@Autowired private FailedPayloadRepository failedPayloadRepository;
+	@Autowired private ProcessingInfoRepository processingInfoRepository;
 
     @BeforeEach
     void setUp(@Autowired JdbcTemplate jdbcTemplate) {
@@ -62,7 +59,7 @@ class InitDummyDataRestControllerTest extends BaseIntegrationTest {
 		
 		await()
 				.atMost(5, TimeUnit.SECONDS)
-				.until(() -> failedPayloadRepository.count() > 0);
+				.until(() -> processingInfoRepository.count() >= 10);
 		
 		assertGenericEventPersisted();
 		assertReceivingEndpointsPersisted();
@@ -70,64 +67,42 @@ class InitDummyDataRestControllerTest extends BaseIntegrationTest {
 		assertMessagesPersisted();
 		assertDeliveryInfosPersisted();
 		assertFailedPayloadPersisted();
+		assertProcessingInfosPersisted();
 	}
 	
 	private void assertGenericEventPersisted() {
 		List<GenericEvent> genericEvents = Lists.newArrayList(genericEventRepository.findAll());
 		assertThat(genericEvents).hasSize(1);
-		assertThat(genericEvents.get(0).getFlowId()).isEqualTo("default-flow");
-		
-		DummyEventPayload payload = JsonUtils.readObjectFromJSON(genericEvents.get(0).getPayloadJson(), DummyEventPayload.class);
-		assertThat(payload.getIntField()).isEqualTo(15);
-		assertThat(payload.getStringField()).isEqualTo("simple string");
 	}
 	
 	private void assertReceivingEndpointsPersisted() {
 		List<RecievingEndpoint> receivingEndpoints = Lists.newArrayList(endpointsRepository.findAll());
 		assertThat(receivingEndpoints).hasSize(3);
-		
-		RecievingEndpoint johnDoeEmail = receivingEndpoints.get(0);
-		assertThat(johnDoeEmail.getEndpointId()).isEqualTo("john.doe@objectify.sk");
-		
-		RecievingEndpoint johnDudlyEmail = receivingEndpoints.get(1);
-		assertThat(johnDudlyEmail.getEndpointId()).isEqualTo("john.dudly@objectify.sk");
-		
-		RecievingEndpoint phone = receivingEndpoints.get(2);
-		assertThat(phone.getEndpointId()).isEqualTo("0918111111");
 	}
 	
 	private void assertNotificationIntentPersisted() {
 		List<NotificationIntent> notificationIntents = Lists.newArrayList(notificationIntentRepository.findAll());
 		assertThat(notificationIntents).hasSize(1);
-		assertThat(notificationIntents.get(0).getBody().getSubject()).isEqualTo("subject");
-		assertThat(notificationIntents.get(0).getBody().getBody()).isEqualTo("body");
-		assertThat(notificationIntents.get(0).getEventIds()).hasSize(1);
 	}
 	
 	private void assertMessagesPersisted() {
 		List<MessagePersistantState> messages = Lists.newArrayList(messageRepository.findAll());
 		assertThat(messages).hasSize(3);
-		assertThat(messages.get(0).getMessageClass()).isEqualTo(EmailMessage.class.getName());
-		assertThat(messages.get(1).getMessageClass()).isEqualTo(EmailMessage.class.getName());
-		assertThat(messages.get(2).getMessageClass()).isEqualTo(SmsMessage.class.getName());
 	}
 	
 	private void assertDeliveryInfosPersisted() {
 		List<DeliveryInfo> deliveryInfos = Lists.newArrayList(deliveryInfoRepository.findAll());
-		assertThat(deliveryInfos).hasSize(7);
-		assertThat(deliveryInfos.get(0).getStatus()).isEqualTo(PROCESSING);
-		assertThat(deliveryInfos.get(1).getStatus()).isEqualTo(SENT);
-		assertThat(deliveryInfos.get(2).getStatus()).isEqualTo(READ);
-		assertThat(deliveryInfos.get(3).getStatus()).isEqualTo(PROCESSING);
-		assertThat(deliveryInfos.get(4).getStatus()).isEqualTo(SENT);
-		assertThat(deliveryInfos.get(5).getStatus()).isEqualTo(READ);
-		assertThat(deliveryInfos.get(6).getStatus()).isEqualTo(FAILED);
+		assertThat(deliveryInfos).hasSize(6);
 	}
 	
 	private void assertFailedPayloadPersisted() {
 		List<FailedPaylod> failedPayloads = Lists.newArrayList(failedPayloadRepository.findAll());
 		assertThat(failedPayloads).hasSize(1);
-		assertThat(failedPayloads.get(0).getErrorMessage()).isEqualTo("illegal argument");
+	}
+	
+	private void assertProcessingInfosPersisted() {
+		List<ProcessingInfo> processingInfos = Lists.newArrayList(processingInfoRepository.findAll());
+		assertThat(processingInfos).hasSize(10);
 	}
 	
 }
